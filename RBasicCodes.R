759677262c4e2e5ffbd314d657b67a128a3c6a53
Parisaghdrzdh <parisaghadirzade@yahoo.com>
2021-12-26 00:33:02 +0330

    R Basic Codes

---
+++ RBasicCodes.R
@@ -0,0 +1,28 @@
+##### R basics by me 
+
+1+1
+2^3
+sqrt(4)
+'hello world'
+1:4
+1:20
+1.5:10
+sum(1:20)
+x<-c(5,12,13)
+x
+y<-c( 'abc', 'de', 'z')
+y
+df<-data.frame(x,y)
+df
+class(df)
+
+x<-c(8,5,12,13)
+g<-c('m','f','m','m')
+x
+g
+
+mean(c(8,12,13))
+
+l<-list(a=c(2,5), b='sky')
+l
+matrix(1:20, 5,4)
